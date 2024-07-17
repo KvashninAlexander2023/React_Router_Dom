103647780e5d9998e0fc1d11262925d4404ca6db
KvashninAlexander2023 <study_python@mail.ru>
2024-07-17 22:24:14 +0500

    test 4

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>,
-
 );
 // hhhggg
 // If you want to start measuring performance in your app, pass a function
